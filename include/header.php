898b5e1966ff6a8bcdb55818ee9382f5994b3e74
deepakmauryacs <62381959+deepakmauryacs@users.noreply.github.com>
2025-08-30 02:35:57 +0530

    Add features page link to header navigation

--- include/header.php
+++ include/header.php
@@ -25,7 +25,7 @@
         </button>
         <div id="nav" class="collapse navbar-collapse">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-lg-center">
-                <li class="nav-item"><a class="nav-link" href="index.php#features">Features</a></li>
+                <li class="nav-item"><a class="nav-link" href="features.php">Features</a></li>
                 <li class="nav-item"><a class="nav-link" href="index.php#how">How it works</a></li>
                 <li class="nav-item"><a class="nav-link" href="pricing.php">Pricing</a></li>
                 <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
