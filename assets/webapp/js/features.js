1b70a312a4836f81d6691f8e80154fb6c2ab5239
deepakmauryacs <62381959+deepakmauryacs@users.noreply.github.com>
2025-08-30 02:28:50 +0530

    Refactor features page into PHP template

---
+++ assets/webapp/js/features.js
@@ -0,0 +1,18 @@
+        // Initialize tooltips
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+        });
+        
+        // Demo interactivity
+        document.querySelectorAll('.form-check-input').forEach(input => {
+            input.addEventListener('change', function() {
+                const card = this.closest('.border');
+                if (this.checked) {
+                    card.classList.add('border-primary');
+                } else {
+                    card.classList.remove('border-primary');
+                }
+            });
+        });
+
