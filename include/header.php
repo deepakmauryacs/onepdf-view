3157dcd08c21fd124bb7b97809d2b60ff56df3b2
Dev <dm.raprocuretesting@gmail.com>
2025-08-30 06:05:56 +0530

    Fix Issue

--- include/header.php
+++ include/header.php
@@ -28,12 +28,12 @@ $current_page = basename($_SERVER['SCRIPT_NAME']);
     </button>
     <div id="nav" class="collapse navbar-collapse">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-lg-center">
-        <li class="nav-item"><a class="nav-link <?= $current_page === 'features.php' ? 'active' : '' ?>" href="features.php">Features</a></li>
+        <li class="nav-item"><a class="nav-link <?= $current_page === 'features.php' ? 'active' : '' ?>" href="features">Features</a></li>
         <li class="nav-item"><a class="nav-link <?= $current_page === 'how-it-works.php' ? 'active' : '' ?>" href="how-it-works.php">How It Works</a></li>
-        <li class="nav-item"><a class="nav-link <?= $current_page === 'pricing.php' ? 'active' : '' ?>" href="pricing.php">Pricing</a></li>
-        <li class="nav-item"><a class="nav-link <?= $current_page === 'contact.php' ? 'active' : '' ?>" href="contact.php">Contact</a></li>
-        <li class="nav-item ms-lg-3"><a class="btn btn-ghost btn-sm <?= $current_page === 'login.php' ? 'active' : '' ?>" href="login.php">Log in</a></li>
-        <li class="nav-item ms-2"><a class="btn btn-brand btn-sm <?= $current_page === 'registration.php' ? 'active' : '' ?>" href="registration.php">Start free</a></li>
+        <li class="nav-item"><a class="nav-link <?= $current_page === 'pricing.php' ? 'active' : '' ?>" href="pricing">Pricing</a></li>
+        <li class="nav-item"><a class="nav-link <?= $current_page === 'contact.php' ? 'active' : '' ?>" href="contact">Contact</a></li>
+        <li class="nav-item ms-lg-3"><a class="btn btn-ghost btn-sm <?= $current_page === 'login' ? 'active' : '' ?>" href="login.php">Log in</a></li>
+        <li class="nav-item ms-2"><a class="btn btn-brand btn-sm <?= $current_page === 'registration' ? 'active' : '' ?>" href="registration.php">Start free</a></li>
       </ul>
     </div>
   </div>
