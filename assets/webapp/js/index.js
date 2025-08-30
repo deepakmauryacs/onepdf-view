8af48c46183bdf477bc021a6663a8f9ba846dde8
Dev <dm.raprocuretesting@gmail.com>
2025-08-30 06:10:46 +0530

    Fix Issue

--- assets/webapp/js/index.js
+++ assets/webapp/js/index.js
@@ -39,24 +39,5 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   }
-
-  // Add active class to nav links on scroll
-  const sections = document.querySelectorAll('section[id]');
-  const navLinks = document.querySelectorAll('.nav-link');
-  window.addEventListener('scroll', () => {
-    let current = '';
-    sections.forEach((section) => {
-      const sectionTop = section.offsetTop;
-      if (pageYOffset >= sectionTop - 100) {
-        current = section.getAttribute('id');
-      }
-    });
-    navLinks.forEach((link) => {
-      link.classList.remove('active');
-      if (link.getAttribute('href').includes(current)) {
-        link.classList.add('active');
-      }
-    });
-  });
 });
 
